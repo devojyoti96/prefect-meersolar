d242d9ce84aef6c3745538e5b4005723ae0d8a5c
devojyoti96 <devojyoti96@gmail.com>
2025-07-23 19:13:12 -0400

    delter

--- prefect_app/start.py
+++
@@ -1,7 +0,0 @@
-import uvicorn
-from prefect.server.api.server import app
-
-if __name__ == "__main__":
-    print("🔌 Launching Prefect API server on 0.0.0.0:4200")
-    uvicorn.run(app, host="0.0.0.0", port=4200)
-
